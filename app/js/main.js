62458018894058c879df93f491874228a4c7ca9a
Sandis M <smortuzans@gmail.com>
2022-03-07 18:35:20 +0200

    1st commit

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.hero__product-slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 2000
+  });
+
+  var mixer = mixitup('.popular__list');
+});
